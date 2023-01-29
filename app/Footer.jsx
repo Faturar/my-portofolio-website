330222fcf7ca91ce625f3f63a4a49702c9177b91
Fatur <64860344+Faturar@users.noreply.github.com>
2023-01-29 21:45:06 +0700

    add link to social media

--- app/Footer.jsx
+++ app/Footer.jsx
@@ -5,23 +5,23 @@ function Footer() {
     <footer className='bg-primary text-white text-center px-12'>
         <div className='flex justify-center items-center py-8 space-x-4'>
             <div className='border py-[7px] px-[10px] rounded-lg'>
-                <Link href={'#'} className='text-xl'>
+                <Link href={'https://www.instagram.com/faturar.dev/'} className='text-xl'>
                     <i className='bx bxl-instagram'></i>
                 </Link>
             </div>
             <div className='border py-[7px] px-[10px] rounded-lg'>
-                <Link href={'#'} className='text-xl'>
+                <Link href={'https://www.facebook.com/faturardev'} className='text-xl'>
                     <i className='bx bxl-facebook'></i>
                 </Link>
             </div>
             <div className='border py-[7px] px-[10px] rounded-lg'>
-                <Link href={'#'} className='text-xl'>
+                <Link href={'www.linkedin.com/in/faturar'} className='text-xl'>
                     <i className='bx bxl-linkedin'></i>
                 </Link>
             </div>
             <div className='border py-[7px] px-[10px] rounded-lg'>
-                <Link href={'#'} className='text-xl'>
-                    <i className='bx bx-envelope' ></i>
+                <Link href={'mailto:faturardev@email.com'} className='text-xl'>
+                    <i className='bx bx-envelope'></i>
                 </Link>
             </div>
         </div>
