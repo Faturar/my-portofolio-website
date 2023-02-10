b8da161de47b715f5fe10aee639be0bbbbb2ba9c
Fatur <64860344+Faturar@users.noreply.github.com>
2023-02-10 18:48:15 +0700

    change title

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -11,15 +11,15 @@ function work() {
       <main className='h-screen w-full text-center overflow-hidden'>
         <div className="container flex flex-col mt-36">
           {/* Header */}
-          <div className="flex flex-col items-center header text-center" data-aos="fade-up" data-aos-duration="1500" data-aos-offset="100">
-              <span className='text-primary font-bold text-lg'>Portfolio</span>
-              <h1 className="text-[40px] leading-tight md:leading-normal font-bold text-main">
-                Under Construction
-              </h1>
-              <p className='text-secondary text-lg text-center md:max-w-[520px]'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, repellendus! Fugiat, labore.
-              </p>
-            </div>
+          <div className="header" data-aos="fade" data-aos-duration="1000" data-aos-offset="100" data-aos-delay="1000">
+            <span className='header-title'>Services</span>
+            <h1 className="header-title-main">
+              Help convert your design
+            </h1>
+            <p className='header-description'>
+              Service that takes a design and converts it into functioning code for a website or application.
+            </p>
+          </div>
         </div>
       </main>
       <Footer />
